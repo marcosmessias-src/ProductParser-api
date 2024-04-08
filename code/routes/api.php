614699be992a7d0a2fe46194fed45722f36902b3
Marcos Messias <marcosmessias1998@gmail.com>
2024-04-08 03:46:39 -0300

    feat: Laravel Breeze installed.

--- code/routes/api.php
+++ code/routes/api.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::controller(ProductController::class)->group(function () {
+Route::controller(ProductController::class)->middleware('auth:api')->group(function () {
 
     /* Route para obter informações da API. */
     Route::get('/', function () {
--- code/routes/console.php
+++ code/routes/console.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Console\Commands\ImportProducts;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
