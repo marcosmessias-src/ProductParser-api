c350cf32ae79221a8c3be439abdc9e286319a635
Marcos Messias <marcosmessias1998@gmail.com>
2024-04-08 14:22:08 -0300

    fix: API routes named.

--- code/routes/api.php
+++ code/routes/api.php
@@ -11,17 +11,17 @@ Route::controller(ProductController::class)->middleware('auth:api')->group(funct
     });
 
     /* Route para pesquisar por produtos. */
-    Route::get('/products-search/{query}', 'search');
+    Route::get('/products-search/{query}', 'search')->name('product-search');
 
     /* Route para atualização de produtos. */
-    Route::put('/products/{code}', 'update');
+    Route::put('/products/{code}', 'update')->name('product-update');
 
     /* Route para deletar um produto específico. */
-    Route::delete('/products/{code}', 'destroy');
+    Route::delete('/products/{code}', 'destroy')->name('product-destroy');
 
     /* Route para exibir informações de um produto específico. */
-    Route::get('/products/{code}', 'show');
+    Route::get('/products/{code}', 'show')->name('product-show');
 
     /* Route para listar todos os produtos com paginação. */
-    Route::get('/products', 'index');
+    Route::get('/products', 'index')->name('product-index');
 });
